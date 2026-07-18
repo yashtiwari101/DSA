616dde5845be1802a6418ee0c3cb07ab250fe7f7
yashtiwari101 <yashtiwarivbn@gmail.com>
2026-07-18 18:51:53 +0530

    selection sort

--- src/Concepts/sorting.java
+++ src/Concepts/sorting.java
@@ -15,9 +15,24 @@ public class sorting {
             }
         }
     }
+    public static void selectionsort(int [] arr){
+        for(int i =0;i<arr.length-1;i++){
+            int minpos = i;
+            for(int j=i+1;j<arr.length;j++){
+                if (arr[minpos]>arr[j]){
+                    minpos=j;
+                }
+            }
+            //swap
+            int temp = arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
+        }
+    }
     public static void main(String[] args) {
         int []arr = {5,4,1,3,2};
-        bubblesort(arr);
+//        bubblesort(arr);
+        selectionsort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
