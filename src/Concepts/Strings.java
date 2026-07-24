1439fdfaac13496661f971cdfac62eb6c4e4c6fa
yashtiwari101 <yashtiwarivbn@gmail.com>
2026-07-24 22:36:06 +0530

    Strings Builder

--- src/Concepts/Strings.java
+++ src/Concepts/Strings.java
@@ -49,6 +49,21 @@ public class Strings {
         }
         return sub;
     }
+    public static String strtouppcase(String str){
+        StringBuilder sb =new StringBuilder();
+        sb.append(Character.toUpperCase(str.charAt(0)));
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }
+            else{
+            sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
 
     public static void main(String arg[]){
 //        char arr[] = {'a','b','c','d'};
@@ -115,6 +130,12 @@ public class Strings {
 //        }
 //        System.out.println(largest);
 
-
+//        StringBuilder sb = new StringBuilder();
+//        for (char ch='a';ch<='z';ch++){
+//            sb.append(ch);
+//        }
+//        System.out.println(sb.length());
+//        String str = "hello world";
+//        System.out.println(strtouppcase(str));
    }
 }
