d8ceca3808e513742b3e0e91000d3316cfdb66ee
yashtiwari101 <yashtiwarivbn@gmail.com>
2026-07-25 16:32:54 +0530

    Bit Manipulation clear last i bits

--- src/Concepts/Bitmanipulation.java
+++ src/Concepts/Bitmanipulation.java
@@ -30,6 +30,19 @@ public class Bitmanipulation {
         int bitmask = ~(1<<i);
         return n & bitmask;
     }
+
+    public static int updateithbit(int n, int i, int bit){
+        if (bit==0){
+            return clearithbit(n,i);
+        }
+        else{
+            return setithbit(n,i);
+        }
+    }
+    public static int clearibits(int n, int i){
+        int bitmask = (~0)<<i;
+        return n & bitmask;
+    }
     public static void main(String[] args) {
 //        System.out.println(5 & 6);
 //        System.out.println(5 | 6);
@@ -40,6 +53,9 @@ public class Bitmanipulation {
 //            primecheck(-6);
 //        System.out.println(ithbit(10,3));
 //        System.out.println(setithbit(10,2));
-        System.out.println(clearithbit(10,1));
+//        System.out.println(clearithbit(10,1));
+//        System.out.println(updateithbit(10,2,1));
+        System.out.println(clearibits(15,2));
+
     }
 }
