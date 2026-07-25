c7c2e05c33d2dd5a2b9602187783b8c210a7301f
yashtiwari101 <yashtiwarivbn@gmail.com>
2026-07-25 15:45:57 +0530

    Bit Manipulation primecheck

--- src/Concepts/Bitmanipulation.java
+++ src/Concepts/Bitmanipulation.java
@@ -2,14 +2,21 @@ package Concepts;
 
 public class Bitmanipulation {
 
-
+    public static void primecheck(int n){
+        if((n & 1)==1){
+            System.out.println("number is odd");
+        }
+        else{
+            System.out.println("number is even");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println(5 & 6);
-        System.out.println(5 | 6);
-        System.out.println(5 ^ 6);
-        System.out.println(~(6));
-        System.out.println(5<<2);
-        System.out.println(2>>1);
-
+//        System.out.println(5 & 6);
+//        System.out.println(5 | 6);
+//        System.out.println(5 ^ 6);
+//        System.out.println(~(6));
+//        System.out.println(5<<2);
+//        System.out.println(2>>1);
+            primecheck(-6);
     }
 }
