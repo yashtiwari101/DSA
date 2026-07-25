48340de0f74935383e94646914ce404523b4e4fe
yashtiwari101 <yashtiwarivbn@gmail.com>
2026-07-25 15:37:32 +0530

    Bit Manipulation operators

--- src/Concepts/Bitmanipulation.java
+++ src/Concepts/Bitmanipulation.java
@@ -7,7 +7,9 @@ public class Bitmanipulation {
         System.out.println(5 & 6);
         System.out.println(5 | 6);
         System.out.println(5 ^ 6);
-
+        System.out.println(~(6));
+        System.out.println(5<<2);
+        System.out.println(2>>1);
 
     }
 }
